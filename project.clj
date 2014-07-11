e97b686dec2a20ae889b9fa95e06d3d302d5f013
stian.alsos@gmail.com <stian.alsos@gmail.com>
2014-07-11 21:17:47 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject webdev "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+  				 [ring "1.3.0"]
+  				 [compojure "1.1.8"]]
+
+:min-lein-version "2.0.0"
+
+:uberjar-name "webdev.jar"
+
+:main webdev.core
+
+:profiles {:dev 
+			{:main webdev.core/-dev-main}})
