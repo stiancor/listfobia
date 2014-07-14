35897061a46f7b84a7e66677de6685c892523cd5
stian.alsos@gmail.com <stian.alsos@gmail.com>
2014-07-14 14:30:50 +0200

    Adding about page and ring handle-dump

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -2,17 +2,23 @@
 	(:require [ring.adapter.jetty :as jetty]
 			  [ring.middleware.reload :refer [wrap-reload]]
 			  [compojure.core :refer [defroutes GET]]
-			  [compojure.route :refer [not-found]]))
+			  [compojure.route :refer [not-found]]
+			  [ring.handler.dump :refer [handle-dump]]))
 
 (defn greet [req] 
 		{:status 200 :body "Hello World!" :headers {}})
+
+(defn about [req] 
+		{:status 200 :body "<h1>About</h1> <p>The only purpose of this site is to learn some web development using Clojure.<p>" :headers {}})
 
 (defn goodbye [req] 
 		{:status 200 :body "Goodbye, Cruel World!" :headers {}})
 
 (defroutes app
 	(GET "/" [] greet)
+	(GET "/about" [] about)
 	(GET "/goodbye" [] goodbye)
+	(GET "/request" [] handle-dump)
 	(not-found "Page not found!"))
 
 (defn -main [port]
